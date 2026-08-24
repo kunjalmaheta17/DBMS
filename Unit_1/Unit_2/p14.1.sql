45d33523522b1c016aeb106cec3c186229bc3ecc
kunjalmaheta17 <mahetakunjal2@gmail.com>
2026-08-24 11:05:02 +0530

    Add files via upload

---
+++ Unit_1/Unit_2/p14.1.sql
@@ -0,0 +1,47 @@
+/*
+Write a function that returns the age, category of an employee.
+Categories:
+● Age < 18 → Minor
+● 18–60 → Adult
+● 60 → Senior
+*/
+
+CREATE OR REPLACE FUNCTION GET_AGE_CATEGORY (
+    P_EID IN NUMBER
+)
+RETURN VARCHAR2
+IS
+    V_AGE EMP.AGE%TYPE;
+    V_CATEGORY VARCHAR2(20);
+BEGIN
+    SELECT AGE
+    INTO V_AGE
+    FROM EMP
+    WHERE EID = P_EID;
+
+    IF V_AGE < 18 THEN
+        V_CATEGORY := 'Minor';
+    ELSIF V_AGE <= 60 THEN
+        V_CATEGORY := 'Adult';
+    ELSE
+        V_CATEGORY := 'Senior';
+    END IF;
+
+    RETURN 'Age: ' || V_AGE || ', Category: ' || V_CATEGORY;
+
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RETURN 'Employee not found';
+END;
+/
+
+/*
+To run this :-
+
+VARIABLE v_eid NUMBER;
+VARIABLE v_result VARCHAR2(100);
+EXEC :v_eid := 101;
+EXEC :v_result := GET_AGE_CATEGORY(:v_eid);
+PRINT v_result;
+
+*/
---
+++ Unit_1/Unit_2/p15.sql
@@ -0,0 +1,4 @@
+/*
+Write a PACKAGE which includes Package Declaration, Package
+Body and call the individual object from the Package.
+*/
